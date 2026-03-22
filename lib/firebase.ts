90dd38c2fc664079a80e68f8be2eb2641b427999
heboah5-tech <heboah5@gmail.com>
2026-03-23 00:46:26 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
- apiKey: "AIzaSyDJOaDkXvm5XP3-Fu0LsuKvQ0_dZK5uf-k",
-  authDomain: "school-77936.firebaseapp.com",
-  databaseURL: "https://school-77936-default-rtdb.firebaseio.com",
-  projectId: "school-77936",
-  storageBucket: "school-77936.firebasestorage.app",
-  messagingSenderId: "783345000886",
-  appId: "1:783345000886:web:585c7579926bcb8f0ca117",
-  measurementId: "G-XX9L099J8H"
+  apiKey: "AIzaSyC4cUitnmd44Dvlp_neV5AmBnXpbBkE8SM",
+  authDomain: "tnmisn-57347.firebaseapp.com",
+  projectId: "tnmisn-57347",
+  storageBucket: "tnmisn-57347.firebasestorage.app",
+  messagingSenderId: "487068100861",
+  appId: "1:487068100861:web:9e8edae073c5d9a69e7694",
+  measurementId: "G-TCL5XN2M47"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
