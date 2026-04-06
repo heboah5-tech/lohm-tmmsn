9c062c650e9639cc4d793c2053289f4f9aeff721
heboah5-tech <heboah5@gmail.com>
2026-04-06 08:21:47 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,6 +5,13 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   // add your firebase config here
+    apiKey: "AIzaSyCo3rP4Mx-zko1L2GrhErHo9QN_PbhF95s",
+  authDomain: "fir-acd64.firebaseapp.com",
+  projectId: "fir-acd64",
+  storageBucket: "fir-acd64.firebasestorage.app",
+  messagingSenderId: "1042218830914",
+  appId: "1:1042218830914:web:847c8200826e8e655971db",
+  measurementId: "G-V4YS80EDX8"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
