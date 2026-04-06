67f9c3af02ff2b8ec4b96d452f59ebb9aa5aa61d
heboah5-tech <heboah5@gmail.com>
2026-04-06 02:14:35 +0000

    remove test config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyBAH7Q_vE79cUiwIcziXJGYNhZj9M_bCF0",
-  authDomain: "sxwq-94f2c.firebaseapp.com",
-  projectId: "sxwq-94f2c",
-  storageBucket: "sxwq-94f2c.firebasestorage.app",
-  messagingSenderId: "970420699631",
-  appId: "1:970420699631:web:f4c514b7d0e053cf3ab61a",
-  measurementId: "G-YSZMVHTMNT"
+  // add your firebase config here
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
